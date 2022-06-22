4f6f685bf1105742ea2e867e8f91322e2968ba26
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-22 15:39:46 +0200

    22 Project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Main />
     </div>
   );
 }
