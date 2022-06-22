d30433cc0c04fd48b6ec26073857c3f9e3a1a349
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-22 16:19:03 +0200

    24 Navbar & Styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Main() {
     return (
-        <h1>Main goes here</h1>
+        <div>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>    
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import logo from '../images/react-logo.png';
 
 function Navbar() {
   return (
-    <h1>Navbar goes here</h1>
+    <nav>
+        <img className='nav--logo' src={logo} alt="Logo"/>
+        <h3 className='nav--logo_text'>ReactFacts</h3>
+        <h4 className='nav--title'>React Course - Project 1</h4>
+    </nav>
   );
 }
 
